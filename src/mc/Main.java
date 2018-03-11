f7421c297873a7a0a1ee8f15bd382cf0c5f5124b
marcel <marcel@localhost.localdomain>
2018-03-11 19:22:28 -0300

    inicio repositorio

---
+++ src/mc/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mc;
+
+/**
+ *
+ * @author marcel
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Comparador c = new Comparador("/run/media/marcel/Marcel-HD/Drivers");
+       
+      // c.abrirDiretorio();
+       
+       //c.mostrarLista();
+       //c.listarArquivosCalcularHash();
+       c.comparar();
+    }
+    
+}
